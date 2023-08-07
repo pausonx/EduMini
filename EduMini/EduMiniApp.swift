b3179beaa5c4ce39f37f2c471451bc6490c9207d
Paulina Wyskiel <p.wyskiel@outlook.com>
2023-08-07 09:22:43 +0200

    Initial Commit

---
+++ EduMini/EduMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduMiniApp.swift
+//  EduMini
+//
+//  Created by Paulina Wyskiel on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct EduMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EduMiniUITests/EduMiniUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EduMiniUITestsLaunchTests.swift
+//  EduMiniUITests
+//
+//  Created by Paulina Wyskiel on 07/08/2023.
+//
+
+import XCTest
+
+final class EduMiniUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
